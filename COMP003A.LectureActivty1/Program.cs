4f9a38dda09388552ebd0200ba67c625bf5793d4
Danny10122 <danieljuarez5575@my.whccd.edu>
2025-01-15 20:58:38 -0800

    10 things about me

--- COMP003A.LectureActivty1/Program.cs
+++ COMP003A.LectureActivty1/Program.cs
@@ -14,6 +14,12 @@ namespace COMP003A.LectureActivty1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi! My name is Daniel Juarez.");
+            Console.WriteLine("Here are 10 things about me");
+            Console.WriteLine("1.I enjoy going out to trips\n2.I'm a fun person to hangout with\n3.I have two pet dogs\n4.I drive a honda\n5.I'm good at soccer\n6.I graduated from Lemoore High\n7.I have 4 siblings\n9.A nickmname people call me is Danny\n10.I have an amazing girlfriend.");
+                string username = Console.ReadLine(); // gets the user input and it to 
+                Console.WriteLine("Hi " + username + ". My name is Daniel");
+
         }
     }
 }
